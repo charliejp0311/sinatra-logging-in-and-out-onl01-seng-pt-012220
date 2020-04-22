d0b5d32a5e219c3c82512145407b5ad93311cf0b
Charles Pelton <charliejp0311@gmail.com>
2020-04-22 17:41:45 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,8 @@
 class Helpers
-  def is_logged_in?(sessions_hash)
-    !!sessions_hash.include?("user_id")
+  def is_logged_in?#(sessions_hash)
+    # !!sessions_hash.include?("user_id")
   end
-  def current_user(sessions_hash)
-    User.find_by_id(sessions_hash[:user_id])
+  def current_user#(sessions_hash)
+    # User.find_by_id(sessions_hash[:user_id])
   end
 end
